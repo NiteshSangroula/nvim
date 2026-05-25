c61e2ecf2e525588f989b910d97bd36fe0606560
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-05-25 13:13:33 +0545

    remove linting from coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -186,31 +186,6 @@ return {
         },
     },
 
-    -- Linting
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufReadPost", "BufWritePost" },
-        config = function()
-            local lint = require("lint")
-
-            -- Linters per filetype
-            -- Add a new language: just add an entry here
-            lint.linters_by_ft = {
-                python     = { "pylint" },
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                java       = { "checkstyle" },
-            }
-
-            -- Trigger linting on these events
-            vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-                callback = function()
-                    lint.try_lint()
-                end,
-            })
-        end,
-    },
-
     -- Auto close brackets
     {
         "windwp/nvim-autopairs",
