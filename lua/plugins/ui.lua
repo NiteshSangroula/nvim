940a9b2968beca3624461136ed401b085cb49528
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-07-23 13:54:56 +0545

    change from gruvbox to tokoyonight->moon

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,15 +1,30 @@
 -- lua/plugins/ui.lua
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "folke/tokyonight.nvim",
     priority = 1000,
     lazy = false,
     opts = {
-      contrast = "hard",
-      transparent_mode = true,
+      style = "moon", -- storm, moon, night, day
+      transparent = false,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl, c)
+        -- annotations tend to blend with keywords by default, so give them
+        -- their own distinct color here
+        hl["@attribute.java"] = { fg = c.orange, bold = true }
+        hl["@attribute"] = { fg = c.orange, bold = true }
+      end,
     },
     init = function()
-      vim.cmd.colorscheme("gruvbox")
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 
